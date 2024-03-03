03ec20cf6f8dfe65e820e20e1f604aaee53f2534
herecomesfed <federicograziani97@gmail.com>
2024-03-03 19:05:57 +0100

    Space Tourism

--- src/components/MediaText/Tabs/MediaTextImg.jsx
+++ src/components/MediaText/Tabs/MediaTextImg.jsx
@@ -1,22 +1,31 @@
 import PropTypes from "prop-types";
 
 const MediaTextImg = ({ bodyClassPage, data, activeTab }) => {
-  const detectSize = (data, activeTab) => {
-    return window.innerWidth < 768
-      ? data[activeTab - 1].images.landscape.replace("./", "./src/")
-      : data[activeTab - 1].images.portrait.replace("./", "./src/");
-  };
-
   return (
     <div className={`${bodyClassPage}__img img-will-change`}>
-      <img
-        src={
-          bodyClassPage !== "technology"
-            ? data[activeTab - 1].images.webp?.replace("./", "./src/")
-            : detectSize(data, activeTab)
-        }
-        alt={data[activeTab - 1].name}
-      />
+      <picture>
+        {bodyClassPage === "technology" && (
+          <>
+            <source
+              srcSet={data[activeTab - 1].images.portrait.replace(
+                "./",
+                "./src/"
+              )}
+              media="(min-width:768px)"
+            />
+            <source
+              srcSet={data[activeTab - 1].images.landscape.replace(
+                "./",
+                "./src/"
+              )}
+            />
+          </>
+        )}
+        <img
+          src={data[activeTab - 1].images.webp?.replace("./", "./src/")}
+          alt={data[activeTab - 1].name}
+        />
+      </picture>
     </div>
   );
 };
